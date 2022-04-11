b9a7c7e9ecdc84e7213e660c7ac202a1b8a34968
Denys Vasylyev <denvasden@gmail.com>
2022-04-11 19:06:04 +0300

    Add modal window

---
+++ sources/javascript/components/ModalWindow.js
@@ -0,0 +1,47 @@
+const MODAL_WINDOW_SELECTOR = ".modal-window";
+const MODAL_WINDOW_CLOSE_SELECTOR = ".modal-window .modal-window_close";
+const MODAL_WINDOW_OPEN_SELECTOR = ".modal-window_open";
+
+class ModalWindow {
+  bodyNode;
+  modalWindowNode;
+  modalWindowCloseNode;
+  modalWindowOpenNodes;
+
+  constructor({ modalWindowSelector, modalWindowCloseSelector, modalWindowOpenSelector } = {}) {
+    this.initialize({ modalWindowSelector, modalWindowCloseSelector, modalWindowOpenSelector });
+    this.initializeListeners();
+  }
+
+  initialize({ modalWindowSelector, modalWindowCloseSelector, modalWindowOpenSelector } = {}) {
+    this.bodyNode = document.querySelector("body");
+    this.modalWindowNode = document.querySelector(modalWindowSelector || MODAL_WINDOW_SELECTOR);
+    this.modalWindowCloseNode = document.querySelector(modalWindowCloseSelector || MODAL_WINDOW_CLOSE_SELECTOR);
+    this.modalWindowOpenNodes = document.querySelectorAll(modalWindowOpenSelector || MODAL_WINDOW_OPEN_SELECTOR)
+  }
+
+  initializeListeners() {
+    this.modalWindowCloseNode.addEventListener("click", this.handleModalWindowClose.bind(this));
+    this.modalWindowOpenNodes.forEach(node => node.addEventListener("click", this.handleModalWindowOpen.bind(this)));
+  }
+
+  handleModalWindowClose() {
+    this.close();
+  }
+
+  handleModalWindowOpen() {
+    this.open();
+  }
+
+  close() {
+    this.bodyNode.classList.remove("modal-window--active");
+    this.modalWindowNode.classList.remove("modal-window--active");
+  }
+
+  open() {
+    this.bodyNode.classList.add("modal-window--active");
+    this.modalWindowNode.classList.add("modal-window--active");
+  }
+}
+
+export { ModalWindow };
--- sources/javascript/index.js
+++ sources/javascript/index.js
@@ -1,10 +1,15 @@
 import Swiper, { Navigation, Pagination } from "swiper";
 
+import { ModalWindow } from "./components/ModalWindow";
+
 import "../stylesheets/scss/index.scss";
 
 class Index {
+  modalWindow;
+  swiperModalWindow;
+  swiperModalWindowNode;
   swiperWhomWeHelped;
-  swiperWhomWeHelpedDOM;
+  swiperWhomWeHelpedNode;
 
   constructor() {
     this.initialize();
@@ -12,12 +17,18 @@ class Index {
 
   initialize() {
     this.initializeSwiperWhomWeHelped();
+    this.initializeSwiperModalWindow();
+    this.initializeModalWindow();
+  }
+
+  initializeModalWindow() {
+    this.modalWindow = new ModalWindow();
   }
 
   initializeSwiperWhomWeHelped() {
-    this.swiperWhomWeHelpedDOM = document.querySelector(".swiper-whom-we-helped");
+    this.swiperWhomWeHelpedNode = document.querySelector(".swiper-whom-we-helped");
 
-    this.swiperWhomWeHelped = new Swiper(this.swiperWhomWeHelpedDOM, {
+    this.swiperWhomWeHelped = new Swiper(this.swiperWhomWeHelpedNode, {
       breakpoints: {
         320: {
           slidesPerView: 1,
@@ -35,13 +46,32 @@ class Index {
       loop: true,
       modules: [Navigation, Pagination],
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".gallery-whom-we-helped .swiper-button-next",
+        prevEl: ".gallery-whom-we-helped .swiper-button-prev",
       },
       pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
+        el: ".swiper-whom-we-helped .swiper-pagination",
+        type: "bullets",
       }
+    });
+  }
+
+  initializeSwiperModalWindow() {
+    this.swiperModalWindowNode = document.querySelector(".swiper-modal-window");
+
+    this.swiperModalWindow = new Swiper(this.swiperModalWindowNode, {
+      loop: true,
+      modules: [Navigation, Pagination],
+      navigation: {
+        nextEl: ".swiper-modal-window .swiper-button-next",
+        prevEl: ".swiper-modal-window .swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-modal-window .swiper-pagination",
+        type: "bullets",
+      },
+      slidesPerView: 1,
+      spaceBetween: 16
     });
   }
 }
